74b48fdc5e240e7e5114fcb49ddf481dde243030
Edouard Desgrée <edouard.desgree@gmail.com>
2023-08-24 21:29:08 +0200

    display message when watchlist is empty

--- src/pages/Watchlist.vue
+++ src/pages/Watchlist.vue
@@ -6,9 +6,11 @@ console.log('myWatchList', store.myWatchList);
 </script>
 <template>
   <h1>My watch list</h1>
-  <ul class="movies-list">
+
+  <ul class="movies-list" v-if="store.myWatchList.length > 0">
     <li v-for="movie in store.myWatchList" :key="movie.imdbID">
       <movie-card :movie="movie" type="watchlist" />
     </li>
   </ul>
+  <p v-else>Nothing to watch yet</p>
 </template>
